f0f9abd0531bc9f88e4080590b9efb7902611aa0
katherine Lundy <katherinersl@gmail.com>
2020-08-29 16:31:10 -0400

    setting up modules and updating github

--- Models/artical.js
+++ Models/artical.js
@@ -1,42 +1,22 @@
 var mongoose = require("mongoose");
+
 var Schema = mongoose.Schema;
-
-var Articleschema = new Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	link: {
-		type: String,
-		required: true,
-	},
-	summary: {
-		type: String,
-		default: "Summary unavailable."
-	},
-	img: {
-		type: String,
-		// default: "/assets/images/unavailable.jpg"
-	},
-	issaved: {
-		type: Boolean,
-		default: false
-	},
-	status: {
-		type: String,
-		default: "Save Article"
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	note: {
-		type: Schema.Types.ObjectId,
-		ref: "Note"
-	}
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+ 
+  comment: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
-
-Articleschema.index({title: "text"});
-
-var Article  = mongoose.model("Article", Articleschema);
+var Article = mongoose.model("Article", ArticleSchema);
 module.exports = Article;
